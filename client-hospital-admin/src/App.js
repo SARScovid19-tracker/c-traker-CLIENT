7ce9713c2e4915720b902fc5af18fbb78aca2af9
rabakar <rifky.akbarmuhammad@gmail.com>
2020-10-20 08:10:38 +0700

    fix initial routing

--- client-hospital-admin/src/App.js
+++ client-hospital-admin/src/App.js
@@ -2,14 +2,15 @@ import React from 'react';
 import { Provider } from 'react-redux'
 import store from './store/index'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import { Home, Login } from './pages'
-// import ProtectedRoute from '../src/components/ProtectedRoute'
+import { Home, Login, Welcome } from './pages'
+import ProtectedRoute from '../src/components/ProtectedRoute'
 
 function App() {
   return (
     <Provider store={store}>
       <Router>
         <Switch>
+          <ProtectedRoute exact path="/" component={Welcome}/>
           <Route path="/login" component={Login}/>
           <Route path="/:hospitalId" component={Home}/>
         </Switch>
---
+++ client-hospital-admin/src/pages/Welcome.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+function Welcome() {
+    return (
+        <div className="d-flex flex-row justify-content-center flex-wrap mt-5 pt-5">
+        <h5 className="text-gray-900 text-2x1 font-semibold mt-5 pt-5">Made with passion and lots of laughter by Wisma, Satriko, Budi, Surya and Rifky ;)</h5>
+        </div>
+    )
+}
+
+export default Welcome
